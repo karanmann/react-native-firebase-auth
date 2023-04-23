ee5d409cfbe6018b8379820cf9e3ed5fa84319ff
Karan Mann <karan.mann@outlook.com>
2023-04-23 22:20:04 +0200

    dev: added user name

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -16,6 +16,7 @@ const HomeScreen = () => {
 
     return (
         <View style={styles.container}>
+            <Text>Hi,{auth.currentUser?.displayName}</Text>
             <Text>Email: {auth.currentUser?.email}</Text>
             <TouchableOpacity
                 onPress={handleSignOut}
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,133 +1,138 @@
-import {FunctionComponent, useEffect, useState} from "react";
-import {KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
-import {auth} from "../firebase";
-import {useNavigation} from "@react-navigation/core";
+import { FunctionComponent, useEffect, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/core";
 
 const LoginScreen: FunctionComponent = () => {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigation = useNavigation();
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.replace("Home");
+      }
+    });
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            if (user) {
-                navigation.replace("Home")
-            }
-        })
+    return unsubscribe;
+  }, []);
 
-        return unsubscribe
-    }, [])
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCreds) => {
+        const user = userCreds.user;
+        const displayName = userName
+        user?.updateProfile({ displayName });
+        console.log("Registered with: ", user?.email);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const handleSignUp = () => {
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then(userCreds => {
-                const user = userCreds.user;
-                console.log('Registered with: ', user?.email);
-            })
-            .catch(error => alert(error.message))
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCrds) => {
+        const user = userCrds.user;
+        console.log("LoggedIn with: ", user?.email);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    }
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior={"padding"}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder={"Name"}
+          style={styles.input}
+          value={userName}
+          onChangeText={(text) => setUserName(text)}
+        ></TextInput>
+        <TextInput
+          placeholder={"Email"}
+          style={styles.input}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        ></TextInput>
+        <TextInput
+          placeholder={"Password"}
+          style={styles.input}
+          value={password}
+          onChangeText={(pwd) => setPassword(pwd)}
+          secureTextEntry
+        ></TextInput>
+      </View>
 
-    const handleLogin = () => {
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then(userCrds => {
-                const user = userCrds.user;
-                console.log('LoggedIn with: ', user?.email);
-            })
-            .catch(error => alert(error.message))
-    };
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
 
-
-    return (
-        <KeyboardAvoidingView
-            style={styles.container}
-            behavior={'padding'}
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
         >
-            <View
-                style={styles.inputContainer}
-            >
-                <TextInput
-                    placeholder={'Email'}
-                    style={styles.input}
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                ></TextInput>
-                <TextInput
-                    placeholder={'Password'}
-                    style={styles.input}
-                    value={password}
-                    onChangeText={pwd => setPassword(pwd)}
-                    secureTextEntry
-                ></TextInput>
-            </View>
-
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    onPress={handleLogin}
-                    style={styles.button}
-                >
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    onPress={handleSignUp}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Register</Text>
-                </TouchableOpacity>
-            </View>
-        </KeyboardAvoidingView>
-        );
-}
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    inputContainer: {
-        width: '80%',
-    },
-    input: {
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-    },
-    buttonContainer: {
-        width: '60%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 40,
-    },
-    button: {
-        backgroundColor: '#0782F9',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-    },
-    buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: '#0782F9',
-        borderWidth: 1,
-    },
-    buttonOutlineText: {
-        color: '#0782F9',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 1,
+  },
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
 });
